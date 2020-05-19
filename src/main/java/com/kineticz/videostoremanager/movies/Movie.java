26b212e7db7f1ef4b3ceccbf973bd76d03874178
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-05-19 23:02:10 +1000

    Remove uuid system

--- src/main/java/com/kineticz/videostoremanager/movies/Movie.java
+++ src/main/java/com/kineticz/videostoremanager/movies/Movie.java
@@ -1,10 +1,6 @@
 package com.kineticz.videostoremanager.movies;
 
-import java.util.UUID;
-
 public class Movie {
-    private UUID uuid;
-
     String title;
     String[] starring;
     String director;
@@ -12,16 +8,10 @@ public class Movie {
     Classification classification;
 
     public Movie(String title, String[] starring, String director, Genre genre, Classification classification){
-        uuid = UUID.randomUUID();
-
         this.title = title;
         this.starring = starring;
         this.director = director;
         this.genre = genre;
         this.classification = classification;
-    }
-
-    public UUID getUUID(){
-        return uuid;
     }
 }

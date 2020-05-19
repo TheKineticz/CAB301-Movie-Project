07b21369c3f20940b47d21fe74dcde800da5a49f
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-05-19 15:24:14 +1000

    Rename main

--- src/main/java/com/kineticz/Main.java
+++
@@ -1,8 +0,0 @@
-package com.kineticz;
-
-public class Main {
-
-    public static void main(String[] args) {
-	
-    }
-}
---
+++ src/main/java/com/kineticz/videostoremanager/VideoStoreManager.java
@@ -0,0 +1,8 @@
+package com.kineticz.videostoremanager;
+
+public class VideoStoreManager {
+
+    public static void main(String[] args) {
+	
+    }
+}

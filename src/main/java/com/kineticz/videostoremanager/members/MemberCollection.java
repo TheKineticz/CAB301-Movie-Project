d698587454cf00325f972e813daf543b98364a2a
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-05-20 11:54:16 +1000

    Create membercollection

--- src/main/java/com/kineticz/videostoremanager/members/MemberCollection.java
+++ src/main/java/com/kineticz/videostoremanager/members/MemberCollection.java
@@ -1,4 +1,56 @@
 package com.kineticz.videostoremanager.members;
 
 public class MemberCollection {
+    private Member[] members;
+    private int end;
+
+    private static final int MAX_MEMBERS = 10;
+
+    public MemberCollection() {
+        members = new Member[MAX_MEMBERS];
+        end = -1;
+    }
+
+    public Member[] getMembers() {
+        return members;
+    }
+
+    public void addMember(Member member) throws Exception {
+        if (end < MAX_MEMBERS - 1){
+            end++;
+            members[end] = member;
+        } else {
+            throw new Exception(String.format("Exceeded maximum member count of %d", MAX_MEMBERS));
+        }
+
+    }
+
+    public boolean removeMember(String username) {
+        for (int i = 0; i <= end; i++) {
+            if (members[i].username.equals(username)) {
+                if (end - 1 - i >= 0) {
+                    System.arraycopy(members, i + 1, members, i, end - 1 - i);
+                }
+
+                end--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int find(String username) {
+        for (int i = 0; i < end; i++) {
+            if (members[i].username.equals(username)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void clear() {
+        members = new Member[MAX_MEMBERS];
+    }
 }

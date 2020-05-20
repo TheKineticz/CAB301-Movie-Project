d01fabccc8f1481929ef20b5c81cf9779ff12844
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-05-20 21:38:39 +1000

    Basic stock tracking for movie objects

--- src/main/java/com/kineticz/videostoremanager/movies/Movie.java
+++ src/main/java/com/kineticz/videostoremanager/movies/Movie.java
@@ -1,17 +1,19 @@
 package com.kineticz.videostoremanager.movies;
 
+import com.kineticz.videostoremanager.exception.MovieStockException;
+
 /**
  * Container for movies and their information
  */
 public class Movie {
-    String title;
-    String[] starring;
-    String director;
-    Genre genre;
-    Classification classification;
+    public final String title;
+    public final String[] starring;
+    public final String director;
+    public final Genre genre;
+    public final Classification classification;
 
-    int stock;
-    int timesBorrowed;
+    private int stock;
+    private int timesBorrowed;
 
     /**
      * Creates a new movie object
@@ -32,5 +34,65 @@ public class Movie {
 
         this.stock = stock;
         timesBorrowed = 0;
+    }
+
+    /**
+     * Get the amount of movie discs in stock
+     *
+     * @return The number of discs in stock
+     */
+    public int getStock() {
+        return stock;
+    }
+
+    /**
+     * Adds n movie discs to the stock
+     *
+     * @param amount The number of movie discs to add
+     * @throws MovieStockException Thrown if the number to be added is zero or negative
+     */
+    public void addStock(int amount) throws MovieStockException {
+        if (amount > 0) {
+            stock += amount;
+        } else {
+            throw new MovieStockException("Cannot add zero or negative stock");
+        }
+    }
+
+    /**
+     * Removes n discs from the stock
+     *
+     * @param amount The number of discs to remove
+     * @throws MovieStockException Thrown if the number of discs to be removed is greater than the amount currently in stock
+     */
+    public void removeStock(int amount) throws MovieStockException {
+        if (amount <= stock) {
+            stock -= amount;
+        } else {
+            throw new MovieStockException("Cannot remove more stock than exists");
+        }
+    }
+
+    /**
+     * Shorthand function for borrowing a single movie disc from the stock
+     *
+     * @return Returns true if the transaction was successful, false if there is not enough stock to complete the transaction
+     */
+    public boolean borrowSingle() {
+        if (stock > 0) {
+            stock--;
+            timesBorrowed++;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Shorthand function for adding a single movie disc to the stock
+     */
+    public void returnSingle() {
+        stock++;
     }
 }

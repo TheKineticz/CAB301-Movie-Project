9e92fb973c7b5a70b78d3560b91ffac0d9f22f28
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-05-20 17:10:31 +1000

    Prep stockkeeping system

--- src/main/java/com/kineticz/videostoremanager/movies/Movie.java
+++ src/main/java/com/kineticz/videostoremanager/movies/Movie.java
@@ -10,11 +10,27 @@ public class Movie {
     Genre genre;
     Classification classification;
 
-    public Movie(String title, String[] starring, String director, Genre genre, Classification classification, int stockQuantity) {
+    int stock;
+    int timesBorrowed;
+
+    /**
+     * Creates a new movie object
+     *
+     * @param title The title of the movie
+     * @param starring An array of the actors starring in the movie
+     * @param director The director of the movie
+     * @param genre The genre of the movie
+     * @param classification The movie's classification
+     * @param stock How many copies of the movie are in stock
+     */
+    public Movie(String title, String[] starring, String director, Genre genre, Classification classification, int stock) {
         this.title = title;
         this.starring = starring;
         this.director = director;
         this.genre = genre;
         this.classification = classification;
+
+        this.stock = stock;
+        timesBorrowed = 0;
     }
 }

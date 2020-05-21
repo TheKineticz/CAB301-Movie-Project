3f7f48fb40339e514fbec7a853eab9c04addd0dc
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2020-05-21 23:00:27 +1000

    Slightly less retarded way of managing the staff account

--- src/main/java/com/kineticz/videostoremanager/members/MemberCollection.java
+++ src/main/java/com/kineticz/videostoremanager/members/MemberCollection.java
@@ -13,10 +13,6 @@ public class MemberCollection {
     private Member[] members;
     private int length;
 
-    private static final String staffUsername = "staff";
-    private static final String staffPasswordSalt = "e488b1eca1e1ccf463d5b2af4aed2504e961586f186389e3baf467ad2838e845";
-    private static final String staffPasswordHash = "134e24e42d6a3bcbb4b1200b95cdaa8c06b421106f46e03c7c7530728fda54d5";
-
     private static final int MAX_MEMBERS = 10;
     private static final Member[] ZERO_MEMBERS = {};
 
@@ -139,16 +135,5 @@ public class MemberCollection {
      */
     public void clear() {
         members = new Member[MAX_MEMBERS];
-    }
-
-    /**
-     * Check the staff login details are correct
-     *
-     * @param username The entered staff username
-     * @param password The entered staff password
-     * @return Returns true if login credentials are correct, false otherwise
-     */
-    public static boolean checkStaffLogin(String username, String password) {
-        return username.equals(staffUsername) && Passwords.checkPassword(password, staffPasswordSalt, staffPasswordHash);
     }
 }
